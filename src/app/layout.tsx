4b4c12a1808072e8b0b202ea2c47a79c81e80099
YoonHaeMin <yunhatmi@gachon.ac.kr>
2025-12-25 16:58:34 +0900

    feat: Integrate Google Analytics component for tracking in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Gamja_Flower, Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import LocationJsProvider from "@/shared/provider/LocationJsProvider";
+import GoogleAnalytics from "@/shared/components/GoogleAnalytics";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -34,6 +35,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
+      <head>
+        <GoogleAnalytics />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${gamjaFlower.variable} antialiased`}
       >
